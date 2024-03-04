fa6d06bbefd41238e427093754fdea633fc4c698
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-03-04 18:28:05 +0100

    25

--- src/main.js
+++ src/main.js
@@ -1,20 +1,2 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper('.swiper-container', {
-    // Настройки Swiper
-    slidesPerView: 1, // Отображаем по одному слайду
-    spaceBetween: 20, // Отступ между слайдами
-    loop: true, // Зацикливание слайдов
-    breakpoints: {
-      // Для широких экранов шире 768px
-      768: {
-        slidesPerView: 2, // Отображаем по два слайда в ряду
-        spaceBetween: 20,
-      },
-      1280: {
-        slidesPerView: 3, // Отображаем по два слайда в ряду
-        spaceBetween: 25,
-      },
-    },
-    // Добавьте другие настройки, если необходимо
-  });
-});
+import './js/slider';
+import './js/mobile-menu';
